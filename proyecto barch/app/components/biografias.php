d7e2d0379b8c305850cee57f6a546a154e9008cd
unknown <guille_gtzg@hotmail.com>
2019-03-12 21:56:51 -0600

    sitio nuevo actualizado

--- proyecto barch/app/components/biografias.php
+++ proyecto barch/app/components/biografias.php
@@ -2,31 +2,31 @@
 <div class="container pt-5">
         <div class="row justify-content-center align-items-center">
           <article class="col-12 col-lg-5 font-120">
-            <h2>Herbert bayer</h2>
+            <h2 >Herbert bayer</h2>
             <p>
             Herbert Bayer (Haag, Austria, 1900 - Santa Bárbara, Estados Unidos, 1985) fue un diseñador gráfico, pintor, fotógrafo y arquitecto austriaco. Bayer fue el diseñador de publicidad más innovador de la Escuela de la Bauhaus, a la que perteneció: su trabajo destaca por la temprana introducción de la fotografía en los años 1920.
             Bayer fue aprendiz del artista George Schmidthammer en Linz. Tras dejar el taller se interesó por el manifiesto Bauhaus de Walter Gropius. Tras estudiar durante cuatro años en la Escuela de la Bauhaus,1​ con profesores como Wassily Kandinsky2​ o László Moholy-Nagy, Gropius nombró a Bayer director de impresión y publicidad. Influenciado por el minimalismo, Bayer desarrolló un estilo visual nítido y adoptó una fuente sans serif completamente en minúsculas para todas las publicaciones de Bauhaus. En 1925 diseñó una fuente sans serif geométrica, universal, cuya versión digital recibe el nombre de Architype Bayer.
             </p>
           </article>
           <article class="col-12 col-lg-7">
-            <img class="img-fluid" src="img/herbert.jpg" alt="¿Necesitas un asombroso sitio web?">
+            <img class="img-fluid" src="img/herr.png" alt="¿Necesitas un asombroso sitio web?">
           </article>
         </div>
       </div>
       </section>
 
       <section>
-<div class="container pt-5">
-        <div class="row justify-content-center align-items-center">
-          <article class="col-12 col-lg-5  order-lg-1 font-120">
+<div class=" container pt-5">
+        <div class="  row justify-content-center align-items-center">
+          <article class=" col-12 col-lg-5  order-lg-1 font-120  ">
             <h2>Lilly Reich</h2>
             <p>
-            Herbert Bayer (Haag, Austria, 1900 - Santa Bárbara, Estados Unidos, 1985) fue un diseñador gráfico, pintor, fotógrafo y arquitecto austriaco. Bayer fue el diseñador de publicidad más innovador de la Escuela de la Bauhaus, a la que perteneció: su trabajo destaca por la temprana introducción de la fotografía en los años 1920.
-            Bayer fue aprendiz del artista George Schmidthammer en Linz. Tras dejar el taller se interesó por el manifiesto Bauhaus de Walter Gropius. Tras estudiar durante cuatro años en la Escuela de la Bauhaus,1​ con profesores como Wassily Kandinsky2​ o László Moholy-Nagy, Gropius nombró a Bayer director de impresión y publicidad. Influenciado por el minimalismo, Bayer desarrolló un estilo visual nítido y adoptó una fuente sans serif completamente en minúsculas para todas las publicaciones de Bauhaus. En 1925 diseñó una fuente sans serif geométrica, universal, cuya versión digital recibe el nombre de Architype Bayer.
-            </p>
+            Lilly Reich (Berlín, 16 de junio de 1885 - Berlín, 14 de diciembre de 1947) fue una diseñadora moderna alemana. Estuvo asociada con Ludwig Mies Van der Rohe durante más de diez años. Fue una de las pocas mujeres profesoras de la Bauhaus.
+            Nacida en Berlín, comenzó su carrera como diseñadora de vestidos de mujer. Esta experiencia desarrolló su interés por el contraste de texturas y materiales, y su aplicación en decoración. Comenzó a trabajar en los talleres de la Wiener Werkstätte en Viena en 1908, con el diseñador modernista Josef Hoffmann, responsable de varias sillas como la Kubus, la Cabinet, la Koller o la Broncia. Allí se formó con Else Oppler-Legband, cofundadora de la Deutscher Werkbund, con quien cursó también estudios en Berlín.
+          </p>
           </article>
           <article class="col-12 col-lg-7">
-            <img class="img-fluid" src="img/lilly.jpg" alt="¿Necesitas un asombroso sitio web?">
+            <img class="img-fluid " src="img/lilly.png" alt="¿Necesitas un asombroso sitio web?">
           </article>
         </div>
       </div>
